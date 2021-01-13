64cc64592fc4b1a4b9e15dc11c7671b31302e894
Flyduckbb <gogo5208@gmail.com>
2021-01-13 19:00:02 +0900

    Bubble_Sort_1

--- Bubble_Sort.c
+++ Bubble_Sort.c
@@ -1,3 +1,36 @@
-#include <stdio.h>
+/*#include <stdio.h>
 #include <stdlib.h>
 
+bubbleSort(int A[], int n)
+{
+	int a;
+	for (int last = n; last > 1; last--)
+	{
+		for (int i = 0; i < last - 1; i++)
+		{
+			if (A[i] > A[i + 1])
+			{
+				a = A[i];
+				A[i] = A[i + 1];
+				A[i + 1] = a;
+			}
+		}
+	}
+}
+
+int main(void)
+{
+	int num[10] = { 3,31,48,73,8,11,20,29,65,15 };
+
+	printf("현재 배열의 상태\n");
+	for(int i=0;i<sizeof(num)/sizeof(int); i++)
+		printf("%d ", num[i]);
+
+	bubbleSort(num, sizeof(num) / sizeof(int));
+
+	printf("\n정렬 후 배열의 상태\n");
+	for (int i = 0; i < sizeof(num) / sizeof(int); i++)
+		printf("%d ", num[i]);
+
+	return 0;
+}*/
