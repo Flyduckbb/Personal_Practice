3708141309c27a1a7bda033dfc501b5c7931dc74
Flyduckbb <gogo5208@gmail.com>
2021-01-23 15:31:04 +0900

    partition_code

---
+++ partition_code.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdio.h>
+
+partition(int B[], int pp, int rr)
+{
+	int x = B[rr];
+	int i = pp - 1;
+	for (int j = pp; j < rr; j++)
+	{
+		if (B[j] <= x)
+		{
+			int a = B[++i];
+			B[i] = B[j];
+			B[j] = a;
+		}
+
+	}
+	int b = B[i + 1];
+	B[i + 1] = B[rr];
+	B[rr] = b;
+
+	return i + 1;
+}
